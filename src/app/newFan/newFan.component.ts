9e76976403320932a4af066ffa399775cf6827d4
MasterKelwin <mkelwin@outlook.com>
2022-09-20 19:02:56 -0300

    data binding and age input on forms

--- src/app/newFan/newFan.component.ts
+++ src/app/newFan/newFan.component.ts
@@ -5,11 +5,19 @@ import { Component } from "@angular/core";
   selector: 'app-newFan',
   templateUrl: './newFan.component.html',
   styleUrls: ['./newFan.component.scss']
-
 })
 
 export class NewFanComponent {
+
+  name: string = '';
+  age: number | undefined;
+  team: string = '';
+
+
   register() {
-    console.log('registered')
+    console.log('registered');
+    console.log(`Nome: ${this.name}`);
+    console.log(`Time: ${this.team}`);
+    console.log(`Idade: ${this.age}`);
   }
 }
