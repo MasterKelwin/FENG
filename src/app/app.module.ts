0497575602c3991b0d10d11c085b3b66a8156705
MasterKelwin <mkelwin@outlook.com>
2022-09-21 09:56:55 -0300

    using pipes for a more friendly user interface, style for table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NewFanComponent } from '../app/newFan/newFan.component'
 import { AppComponent } from './app.component';
 import { FansComponent } from './fans/fans.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -15,7 +19,13 @@ import { FansComponent } from './fans/fans.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'}, 
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
